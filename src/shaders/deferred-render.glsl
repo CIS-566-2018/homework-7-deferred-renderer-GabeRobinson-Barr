93e8f76a4eb207b2626397880ed5a65864cb8fe6
GabeRobinson-Barr <grobi@seas.upenn.edu>
2018-03-27 02:33:42 -0400

    Finished tonemapping

--- src/shaders/deferred-render.glsl
+++ src/shaders/deferred-render.glsl
@@ -23,16 +23,16 @@ void main() {
 	// Blinn-Phong Shading
 	vec4 gb0 = texture(u_gb0, fs_UV);
 
-	vec3 lightDir = normalize(vec3(-1, -1, 1));
-	float ambient = 0.1;
-	float lambert = clamp(dot(lightDir, gb0.xyz), 0.0, 1.0) + ambient; // Get the lambertian term
+	vec3 lightDir = normalize(vec3(1, 1, 1));
+	float ambient = 0.05;
+	float lambert = clamp(dot(lightDir, normalize(gb0.xyz)), 0.0, 1.0) + ambient; // Get the lambertian term
 
 	vec2 ndc = (2.0 * fs_UV) - 1.0;
 	vec3 modelpos = vec3(u_CamPos) + gb0.w * vec3(1,0,0) * tan(ndc.x/gb0.w) + gb0.w * vec3(0,1,0) * tan(ndc.y/gb0.w) + gb0.w * vec3(0,0,1);
 	vec3 look = normalize(modelpos - vec3(u_CamPos));
 
-	vec3 H = normalize(lightDir + look);
-	float specular = pow(abs(dot(H, gb0.xyz)), 16.0);
+	vec3 H = normalize(-lightDir + look);
+	float specular = pow(abs(dot(normalize(gb0.xyz), H)), 30.0);
 
 	// Base Color
 	vec4 gb2 = texture(u_gb2, fs_UV);
@@ -53,9 +53,18 @@ void main() {
 	float t = clamp(mod(u_Time / 5.0, 2.0), 0.0, 1.0);
 	t = pow(4.0 * t * (1.0 - t), 4.0) + 0.01;
 
-	vec3 backbase = step(-0.1, raydir.y) * vec3(0.2, 0.2, 0.8) * t + step(raydir.y, -0.1) * vec3(0.36, 0.6, 0.18) * (t + 0.2);	
+	vec3 backbase = step(-0.1, raydir.y) * vec3(0.16, 0.16, 0.7) * t + step(raydir.y, -0.1) * vec3(0.3, 0.5, 0.12) * (t + 0.2);	
 	vec3 backcol = ismesh * backbase;
 
-	out_Col = vec4(lambert * col + specular + backcol, 1.0);
-	//out_Col = vec4(col, 1.0);
+	vec3 meshcol = col * lambert;
+
+//#define BLINN_PHONG
+#ifdef BLINN_PHONG
+	meshcol += specular;
+#endif
+
+	meshcol = clamp(meshcol * (1.0 - ismesh), 0.0, 1.0);
+
+	out_Col = vec4(meshcol + backcol, 1.0);
+
 }
--- src/shaders/tonemap-frag.glsl
+++ src/shaders/tonemap-frag.glsl
@@ -15,9 +15,16 @@ void main() {
 	// It does not properly handle HDR values; you must implement that.
 
 	vec3 color = texture(u_frame, fs_UV).xyz;
-	color = min(vec3(1.0), color);
+	//color = min(vec3(1.0), color); // Unneeded after hdr tone mapping
+
+	// Tone Mapping
+	color *= 4.0; // Using an exposure value of 4 because 16 is way too high
+	
+	vec3 x = max(vec3(0.0), color - 0.004);
+	color = (x * (6.2 * x + 0.5)) / (x * (6.2 * x + 1.7) + 0.06);
 
 	// gamma correction
-	color = pow(color, vec3(1.0 / 2.2));
+	//color = pow(color, vec3(1.0 / 2.2)); // Dont need this for this version of tone mapping
+
 	out_Col = vec4(color, 1.0);
 }
